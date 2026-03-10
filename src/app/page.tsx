db2d17b2994fa3fe70a05b8360621af93c8fb6bc
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-03-10 15:22:47 -0300

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const cookieStore = await cookies()
 
